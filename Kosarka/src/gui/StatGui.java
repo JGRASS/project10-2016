3c22c810ae873d5a229260ada74e1e25a8ef9ecb
LukaOrlovic <orlovicluka@hotmail.com>
2016-05-08 14:16:22 +0200

    Popravljeno dugme za izlazak iz prozora "Statistika".

--- Kosarka/src/gui/StatGui.java
+++ Kosarka/src/gui/StatGui.java
@@ -17,6 +17,8 @@ import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import javax.swing.JTable;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
@@ -28,6 +30,8 @@ import javax.swing.JRadioButton;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * 
@@ -42,13 +46,21 @@ public class StatGui extends JFrame {
 	 * Create the frame.
 	 */
 	public StatGui() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				if(GUIKontroler.daLiSteSigurniDaZeliteDaOdustanete() == JOptionPane.YES_OPTION){
+					dispose();
+				}
+			}
+		});
 		
 		txtKosevi = new JTextField();
 		txtKosevi.setEditable(false);
 		txtKosevi.setText("Kosevi");
 		txtKosevi.setBounds(152, 11, 86, 20);
 		txtKosevi.setColumns(10);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
 		
